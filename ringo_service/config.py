fbd6ce77b9ee7ce63ba205cee13bca28a9cb7f74
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-09-19 14:37:17 +0200

    Added config object with default configuration options.

---
+++ ringo_service/config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Service configuration
+See http://flask.pocoo.org/docs/0.12/config/
+"""
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'sqlite://:memory:'
+    API_CONFIG = '../swagger/api.yaml'
+    SERVER = 'gevent'
+    SERVER_PORT = 8080
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SERVER = None
+
+
+class TestingConfig(Config):
+    TESTING = True
