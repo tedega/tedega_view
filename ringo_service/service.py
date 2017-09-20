ac083961ec0e4d77d55ae05f60fba1212c5aa667
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-09-20 14:49:20 +0200

    Implemented first version of the servive.
    The version is capable of doing CRUD on a real database.

--- ringo_service/service.py
+++ ringo_service/service.py
@@ -4,20 +4,41 @@ import logging
 import connexion
 from connexion import NoContent
 
+from db import get_engine, get_session
+from model.item import (
+    create_model,
+    load_items,
+    load_item,
+    create_item
+)
+
+from model.converters import (
+    from_json,
+    to_json
+)
+
+
 # Name of the environment valirable which stores the path to the custom
 # service configuration. See
 # http://flask.pocoo.org/docs/dev/config/#configuring-from-files
 SERVICE_CONFIG = "SERVICE_CONFIG"
 
-# Our memory-only item storage
-ITEMS = {}
-
 # Create a new logger for this service.
 logger = logging.getLogger(__name__)
 
 
+connexion_app = connexion.App(__name__)
+app = connexion_app.app
+config = app.config
+config.from_object('ringo_service.config.DevelopmentConfig')
+if os.environ.get(SERVICE_CONFIG):
+    config.from_envvar(SERVICE_CONFIG)
+
+db = get_session(config.get('DATABASE_URI'))
+
+
 def get_items(limit):
-    return [item for item in ITEMS.values()][:limit]
+    return [to_json(item.values) for item in load_items(db)][:limit]
 
 
 def get_item(item_id):
@@ -26,33 +47,50 @@ def get_item(item_id):
 
 
 def put_item(item_id, item):
-    exists = item_id in ITEMS
-    if exists:
-        logger.info('Updating item %s..', item_id)
-        ITEMS[item_id].update(item)
+    loaded_item = load_item(db, item_id)
+    if loaded_item:
+        try:
+            loaded_item.set_values(from_json(item))
+            db.commit()
+            logger.info('Updating item %s..', item_id)
+            return NoContent, 200
+        except Exception:
+            logger.error('Failed updating item %s..', item_id)
+            db.rollback()
+            raise
     else:
-        logger.info('Creating item %s..', item_id)
-        ITEMS[item_id] = item
-    return NoContent, (200 if exists else 201)
+        try:
+            new_item = create_item(from_json(item))
+            db.add(new_item)
+            db.commit()
+            logger.info('Creating item %s..', item_id)
+            return NoContent, 201
+        except Exception:
+            logger.error('Failed creating item %s..', item_id)
+            db.rollback()
+            raise
 
 
 def delete_item(item_id):
-    if item_id in ITEMS:
-        logger.info('Deleting item %s..', item_id)
-        del ITEMS[item_id]
-        return NoContent, 204
+    loaded_item = load_item(db, item_id)
+    if loaded_item:
+        try:
+            db.delete(loaded_item)
+            db.commit()
+            logger.info('Deleting item %s..', item_id)
+            return NoContent, 204
+        except Exception:
+            db.rollback()
+            logger.error('Failed deleting item %s..', item_id)
+            raise
     else:
         return NoContent, 404
 
 if __name__ == '__main__':
-    connexion_app = connexion.App(__name__)
-    app = connexion_app.app
 
     # Load configuration
-    config = app.config
-    config.from_object('ringo_service.config.DevelopmentConfig')
-    if os.environ.get(SERVICE_CONFIG):
-        config.from_envvar(SERVICE_CONFIG)
+    engine = get_engine(app.config.get("DATABASE_URI"))
+    create_model(engine)
 
     # Setup Logging
     if config.get("DEBUG"):
