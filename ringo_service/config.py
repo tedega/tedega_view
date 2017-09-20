0269ecda6e260376ae4349bfffc24e8fb4674a31
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-09-20 20:48:29 +0200

    Added documentation on configuration variable

--- ringo_service/config.py
+++ ringo_service/config.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 """
 Service configuration
 See http://flask.pocoo.org/docs/0.12/config/
@@ -10,22 +8,25 @@ See http://flask.pocoo.org/docs/0.12/config/
 
 class Config(object):
     DEBUG = False
-    TESTING = False
+    """Set to true to enable debug logging"""
     DATABASE_URI = 'sqlite://:memory:'
+    """Configure the database connection."""
     API_CONFIG = '../swagger/api.yaml'
+    """Path to the API configuration used by connexion."""
+    DOMAIN_MODEL = ''
+    """Path to the root class of your domain mode. E.g.
+    `path.to.your.domain.Class`."""
     SERVER = 'gevent'
+    """Configure which server should be used. Default to werkzeug."""
     SERVER_PORT = 8080
+    """Configure the port on which the server will listen."""
 
 
 class ProductionConfig(Config):
-    DATABASE_URI = 'mysql://user@localhost/foo'
+    DATABASE_URI = 'sqlite:///db.sqlite'
 
 
 class DevelopmentConfig(Config):
     DATABASE_URI = 'sqlite:///db.sqlite'
     DEBUG = True
     SERVER = None
-
-
-class TestingConfig(Config):
-    TESTING = True
