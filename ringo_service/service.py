fd4c50937a59ac06761d56a04fafc6fd13b12c3e
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-10-06 16:07:09 +0200

    Added our own route resolver which always returns a generic endpoint.

--- ringo_service/service.py
+++ ringo_service/service.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+import sys
 import logging
 import venusian
 import connexion
+from connexion.resolver import Resolver
+from connexion.exceptions import ResolverError
 
 from lib.swagger import (
     write_config,
     generate_config
 )
 from api import (
-    registry
+    registry, generic
 )
 
 SERVICE_CONFIG = "SERVICE_CONFIG"
@@ -28,13 +31,31 @@ application. The following modes are available:
 logger = logging.getLogger(__name__)
 
 
+class ServiceResolver(Resolver):
+
+    def __init__(self):
+        self.function_resolver = lambda x: generic
+
+    def resolve_function_from_operation_id(self, operation_id):
+        """
+        Invokes the function_resolver
+        :type operation_id: str
+        """
+        try:
+            return self.function_resolver(operation_id)
+        except ImportError as e:
+            msg = 'Cannot resolve operationId "{}"! Import error was "{}"'.format(operation_id, str(e))
+            raise ResolverError(msg, sys.exc_info())
+        except (AttributeError, ValueError) as e:
+            raise ResolverError(str(e), sys.exc_info())
+
+
 def start_service(swagger_config, modul, port=None, server=None):
     scanner = venusian.Scanner(registry=registry)
     scanner.scan(modul)
 
     # Generate the config file
     swagger_config = generate_config(swagger_config, registry)
-
 
     connexion_app = connexion.App(__name__)
     config = connexion_app.app.config
@@ -51,5 +72,5 @@ def start_service(swagger_config, modul, port=None, server=None):
         logging.basicConfig(level=logging.INFO)
 
     with write_config(swagger_config) as swagger_config_file:
-        connexion_app.add_api(swagger_config_file)
+        connexion_app.add_api(swagger_config_file, resolver=ServiceResolver())
     connexion_app.run(port=port, server=server)
