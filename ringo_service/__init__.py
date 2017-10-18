d37d5655e0f250e7bba544c047e1e2c66e23f078
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-10-18 14:40:05 +0200

    Define __all__ in __init.__.py
    This is recomend way to define what happens when someone import * from ringo_service and prevents pyflakes complaining about unused imports.

--- ringo_service/__init__.py
+++ ringo_service/__init__.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from .registry import (  # noqa
+from .registry import (
     config_service_endpoint,
     config_service_model
 )
-from .exceptions import (  # noqa
+from .exceptions import (
     NotFound,
     ClientError
 )
-from .service import start_service  # noqa
+from .service import start_service
+
+__all__ = [config_service_endpoint, config_service_model,
+           NotFound, ClientError,
+           start_service]
 
 __author__ = """Torsten Irländer"""
 __email__ = 'torsten.irlaender@googlemail.com'
