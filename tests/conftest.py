6ad51acecc8998f4be8aecd4e6831b27f90c369e
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-12-13 12:13:48 +0100

    Initialise logger on test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import os
 import pytest
 import tedega_view
+import tedega_share
 
 
 @pytest.fixture
 def app():
     swagger_config = os.path.abspath(os.path.join(os.path.dirname(__file__), "swagger.yaml"))
     tedega_view.server.register_endpoints(tedega_view)
-    app = tedega_view.server.create_server(tedega_view, swagger_config)
+    run_on_init = [(tedega_share.init_logger, "tedega_view")]
+    app = tedega_view.server.create_application("tedega_view", swagger_config, run_on_init=run_on_init)
     return app.app
