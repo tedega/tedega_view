998c27c683560d2897ca55933bad87c69ac131ba
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-12-14 13:51:03 +0100

    Added tests for building parameters for endpoints.

--- tests/test_tedega_view.py
+++ tests/test_tedega_view.py
@@ -7,6 +7,7 @@ test_tedega_view
 
 Tests for `tedega_view` module.
 """
+import pytest
 
 
 def test_version(client):
@@ -23,3 +24,42 @@ def test_version(client):
     assert res.status_code == 404
     res = client.get('/test?action=genericerror')
     assert res.status_code == 500
+
+
+def test_get_endpoint():
+
+    def xxx(action):
+        pass
+
+    from tedega_view.views import _get_endpoint_parameter
+
+    # There is a value for the action parameter
+    result = _get_endpoint_parameter(xxx, {"action": "version"})
+    assert result == {'action': 'version'}
+
+    # There is a value for the action parameter as encoded JSON.
+    with pytest.raises(ValueError):
+        result = _get_endpoint_parameter(xxx, bytes("{\"action\": \"version\"}", "utf-8"))
+
+    # There is a no value for the action parameter
+    result = _get_endpoint_parameter(xxx, {"notpresent": "version"})
+    assert result == {}
+
+    # There is a value for the action parameter with in the nested
+    # structure.
+    result = _get_endpoint_parameter(xxx, {"values": {"action": "version"}})
+    assert result == {'action': 'version'}
+
+    # There is a not value for the action parameter with in the nested
+    # structure.
+    result = _get_endpoint_parameter(xxx, {"values": {"notpresent": "version"}})
+    assert result == {}
+
+    # There is a value for the action parameter with in the nested
+    # structure endcoded as JSON.
+    result = _get_endpoint_parameter(xxx, {"values": bytes("{\"action\": \"version\"}", "utf-8")})
+    assert result == {'action': 'version'}
+
+    # There is no value for the action parameter
+    result = _get_endpoint_parameter(xxx, {"values": bytes("foobar", "utf-8")})
+    assert result == {}
